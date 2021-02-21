41af75451cb389705537ae1c9e4921e831f88e4e
kimhyeyun <cqqwer@gmail.com>
2021-02-21 16:57:41 +0900

    WEEK 2 종이조각// 너무 어렵...;;

--- 2021AlgorithmStudy/비트마스크_14391.cpp
+++ 2021AlgorithmStudy/비트마스크_14391.cpp
@@ -1,94 +1,77 @@
-#include<iostream>
-#include<string>
-#include<vector>
+	#include<iostream>
+	using namespace std;
 
-#define MAX 4
-using namespace std;
+	int N, M, ans;
+	int Rectangle[4][4];
+	bool visited[4][4] = { false, };
 
-int N, M, ans;
-int MAP[MAX][MAX];
-int Select[16];
-bool visited[MAX][MAX];
-vector<int> v;
+	void Solve(int n, int sum) {
+		int x = n / M;
+		int y = n % M;
 
-//못하겠다 ... 어렵다....
-void Make_Visit(int x,int y, int size,char c, bool flag) {
-    if (c == 'G') {
-        for (int i = y; i < y + size; i++)
-            visited[x][i] = flag;
-    }
-    else {
-        for (int i = x; i < x + size; i++)
-            visited[y][i] = flag;
-    }
-}
+		if (n >= N * M) {
+			ans = max(ans, sum);
+			return;
+		}
 
-int Find_Max_Size(int x, int y, char c) {
-    int cnt = 0;
-    if (c == 'G') { //가로
-        for (int i = y; i < M; i++) {
-            if (!visited[x][i])
-                cnt++;
-            else
-                break;
-        }
-    }
-    else {
-        for(int i=x;i<N;i++){
-            if (!visited[i][y])
-                cnt++;
-            else
-                break;
-        }
-    }
-    return cnt;
-}
+		if (visited[x][y])
+			Solve(n + 1, sum);
 
-pair<int, int> Find_Point() {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++)
-            if (!visited[i][j])
-                return { i,j };
-    }
-    return { -1,-1 };
-}
+		else {
+			int tmp, nx, ny;
+			tmp = Rectangle[x][y];
+			visited[x][y] = true;
+			Solve(n + 1, sum + tmp);
+			visited[x][y] = false;
 
-void DFS(int sum) {
-    if (N == 1 && M == 1) {
-        cout << MAP[0][0] << "\n";
-        return;
-    }
+			//밑으로
+			for (int i = 1; i < N - x; i++) {
+				nx = x + i;
+				tmp *= 10;
+				tmp += Rectangle[nx][y];
 
-    pair<int, int> p = Find_Point();
-    int x = p.first;
-    int y = p.second;
+				for (int j = 1; j <= i; j++)
+					visited[x + j][y] = true;
 
-    if (x == -1 && y == -1) {
-        if (ans < sum)
-            ans = sum;
-        return;
-    }
+				Solve(n + 1, sum + tmp);
 
-    int sz = Find_Max_Size(x, y, 'G');
+				for (int j = 1; j <= i; j++)
+					visited[x + j][y] = false;
+			}
 
-    for (int i = sz; i > 0; i--) {
-        Make_Visit(x, y, sz, 'G', true);
-    }
-}
+			tmp = Rectangle[x][y];
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+			//오른쪽으로
+			for (int i = 1; i < M - y; i++) {
+				ny = y + i;
+				if (visited[x][ny])
+					return;
+				tmp *= 10;
+				tmp += Rectangle[x][ny];
 
-    cin >> N >> M;
+				for (int j = 1; j <= i; j++)
+					visited[x][y + j] = true;
 
-    for (int i = 0; i < N; i++) {
-        string str;
-        cin >> str;
-        for (int j = 0; j < str.length(); j++)
-            MAP[i][j] = str[j] - '0';
-    }
+				Solve(n + 1, sum + tmp);
 
+				for (int j = 1; j <= i; j++)
+					visited[x][y + j] = false;
+			}
 
-}
+		}
+	}
+
+	int main() {
+		string s;
+		cin >> N >> M;
+
+		for (int i = 0; i < N; i++) {
+			cin >> s;
+			for (int j = 0; j < M; j++)
+				Rectangle[i][j] = s[j] - '0';
+		}
+
+		Solve(0, 0);
+
+		cout << ans << "\n"; 
+	}
